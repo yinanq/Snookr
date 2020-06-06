fb53ed87e5ca2122b72c403d0872aa450f220740
Yinan <yinancode@gmail.com>
2020-06-05 17:45:35 -0700

    scrollview for about tab body section

--- Snookr/Snookr/View Controllers/InfoTab/InfoVC.swift
+++ Snookr/Snookr/View Controllers/InfoTab/InfoVC.swift
@@ -11,7 +11,7 @@ import UIKit
 class InfoVC: UIViewController {
     
     let logoImageView = UIImageView()
-    var bodyTextViewContainerView = UIView()
+    var bodyScrollView = UIScrollView()
     var bodyTextView = SNKTextView()
 
     override func viewDidLoad() {
@@ -30,7 +30,7 @@ class InfoVC: UIViewController {
     }
     
     private func configureBodyTextViewContainerView() {
-        let string = "Snookr is a snooker scoreboard and a calculator of how many snookers are required. You and your opponent at the snooker table can each use your own Snookr app on your own iPhone in the Bluetooth-connected mode. You can also choose not to turn on Bluetooth connection and just update both players' scores on one iPhone.\nSnookr is designed and developed by Yinan Qiu. Feel free to contact him if you have feedback, questions, or feature requests. Last but not least, he wishes you loads of ranking points, mate!"
+        let string = "The Scoreboard\nTo get the most out of this scoreboard, you and your opponent at the snooker table can each use your own Snookr app on your own iPhone in the Bluetooth-connected mode. Of course, you can also choose not to turn on Bluetooth connection and just update both players' scores from one Snookr app on one iPhone.\nThe Snookers Calculator\nThe Snookers tab is where you can check how many snookers are required when there is not enough points left on the table. It can be used with or without the scoreboard part of Snookr.\nThe Author\nSnookr is designed and developed by Yinan Qiu. Feel free to contact him if you have feedback, questions, or feature requests. Last but not least, he wishes you loads of ranking points, mate!"
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = SNKPadding.lineSpacing
         paragraphStyle.alignment = .left
@@ -41,13 +41,14 @@ class InfoVC: UIViewController {
             .foregroundColor: SNKColor.foregroundGray,
         ]
         let maString = NSMutableAttributedString(string: string, attributes: attributes)
-        maString.addAttribute(.link, value: "https://yinan.design/contact", range: NSRange(location: 379, length: 11))
+        maString.addAttribute(.link, value: "https://yinan.design/contact", range: NSRange(location: 601, length: 11))
         bodyTextView.isEditable = false
         bodyTextView.linkTextAttributes = [NSAttributedString.Key.underlineStyle: true]
         bodyTextView.attributedText = maString
-        bodyTextViewContainerView.translatesAutoresizingMaskIntoConstraints = false
-        bodyTextViewContainerView.addSubview(bodyTextView)
-        view.addSubview(bodyTextViewContainerView)
+        bodyScrollView.translatesAutoresizingMaskIntoConstraints = false
+//        bodyScrollView.
+        bodyScrollView.addSubview(bodyTextView)
+        view.addSubviews(logoImageView, bodyScrollView)
     }
     
     @objc func didTapCloseButton() { dismiss(animated: true) }
@@ -55,14 +56,16 @@ class InfoVC: UIViewController {
     private func layout() {
         NSLayoutConstraint.activate([
             logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
-            logoImageView.leadingAnchor.constraint(equalTo: bodyTextViewContainerView.leadingAnchor),
-            bodyTextViewContainerView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 30),
-            bodyTextViewContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: SNKPadding.big),
-            bodyTextViewContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -SNKPadding.big),
-            bodyTextView.topAnchor.constraint(equalTo: bodyTextViewContainerView.topAnchor),
-            bodyTextView.leadingAnchor.constraint(equalTo: bodyTextViewContainerView.leadingAnchor),
-            bodyTextView.trailingAnchor.constraint(equalTo: bodyTextViewContainerView.trailingAnchor),
-            bodyTextView.bottomAnchor.constraint(equalTo: bodyTextViewContainerView.bottomAnchor),
+            logoImageView.leadingAnchor.constraint(equalTo: bodyScrollView.leadingAnchor),
+            bodyScrollView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 35),
+            bodyScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: SNKPadding.big),
+            bodyScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -SNKPadding.big),
+            bodyScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -SNKPadding.big),
+            bodyTextView.topAnchor.constraint(equalTo: bodyScrollView.topAnchor),
+            bodyTextView.leadingAnchor.constraint(equalTo: bodyScrollView.leadingAnchor),
+            bodyTextView.trailingAnchor.constraint(equalTo: bodyScrollView.trailingAnchor),
+            bodyTextView.widthAnchor.constraint(equalTo: bodyScrollView.widthAnchor),
+            bodyTextView.bottomAnchor.constraint(equalTo: bodyScrollView.bottomAnchor)
         ])
     }
     
