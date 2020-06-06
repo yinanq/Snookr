51044a8954d0a0eafc8cdce583d6a0e8f890d83e
Yinan <yinancode@gmail.com>
2020-06-06 12:37:12 -0700

    background blur with scroll past bounds effect for About tab

--- Snookr/Snookr/View Controllers/InfoTab/InfoVC.swift
+++ Snookr/Snookr/View Controllers/InfoTab/InfoVC.swift
@@ -10,27 +10,22 @@ import UIKit
 
 class InfoVC: UIViewController {
     
+    let headerView = UIVisualEffectView()
     let logoImageView = UIImageView()
     var bodyScrollView = UIScrollView()
     var bodyTextView = SNKTextView()
-//    let gradientView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .dark
-        view.backgroundColor = SNKColor.background
-        configureTitle()
-        configureBodyTextViewContainerView()
-        layout()
+        configureChildViews()
     }
     
-    private func configureTitle() {
-        logoImageView.image = UIImage(named: "logo-green")
+    private func configureChildViews() {
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.effect = UIBlurEffect(style: .regular)
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(logoImageView)
-    }
-    
-    private func configureBodyTextViewContainerView() {
+        logoImageView.image = UIImage(named: "logo-green")
         let string = "The Scoreboard\nTo get the most out of this scoreboard, you and your opponent at the snooker table can each use your own Snookr app on your own iPhone in the Bluetooth-connected mode. Of course, you can also choose not to turn on Bluetooth connection and just update both players' scores from one Snookr app on one iPhone.\nThe Snookers Calculator\nThe Snookers tab is where you can check how many snookers are required when there is not enough points left on the table. It can be used with or without the scoreboard part of Snookr.\nThe Engineer\nSnookr is designed and developed by Yinan Qiu. Feel free to contact him if you have feedback, questions, or feature requests. Last but not least, he wishes you loads of ranking points, mate!\n\n"
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = SNKPadding.lineSpacingSmall
@@ -52,31 +47,19 @@ class InfoVC: UIViewController {
         bodyTextView.attributedText = maString
         bodyScrollView.translatesAutoresizingMaskIntoConstraints = false
         bodyScrollView.addSubview(bodyTextView)
-        view.addSubview(bodyScrollView)
-//        let gradientLayer = CAGradientLayer()
-//        gradientLayer.colors = [SNKColor.background.withAlphaComponent(0).cgColor, SNKColor.background.withAlphaComponent(0.9).cgColor]
-//        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
-//        gradientLayer.endPoint = CGPoint(x: 0, y: 1)
-//        let height: CGFloat = 90
-//        let bottom = tabBarController?.tabBar.frame.size.height ?? 0
-//        gradientView.frame = CGRect(x: SNKPadding.big, y: view.bounds.height - height - bottom, width: view.bounds.width - 2*SNKPadding.big, height: height)
-//        gradientLayer.frame = gradientView.bounds
-//        gradientView.layer.addSublayer(gradientLayer)
-//        gradientView.isUserInteractionEnabled = false
-//        view.addSubview(gradientView)
-//        gradientView.layer.borderColor = SNKColor.foreground.cgColor
-    }
-    
-    @objc func didTapCloseButton() { dismiss(animated: true) }
-    
-    private func layout() {
+        view.addSubviews(bodyScrollView, headerView, logoImageView)
+        bodyScrollView.clipsToBounds = false
         NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.bottomAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: SNKPadding.big),
             logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
             logoImageView.leadingAnchor.constraint(equalTo: bodyScrollView.leadingAnchor),
             bodyScrollView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 35),
             bodyScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: SNKPadding.big),
             bodyScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -SNKPadding.big),
-            bodyScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -15),
+            bodyScrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -SNKPadding.big),
             bodyTextView.topAnchor.constraint(equalTo: bodyScrollView.topAnchor),
             bodyTextView.leadingAnchor.constraint(equalTo: bodyScrollView.leadingAnchor),
             bodyTextView.trailingAnchor.constraint(equalTo: bodyScrollView.trailingAnchor),
